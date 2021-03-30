68ae07ac66b8a51ace9456c65398bf25b8b1ba7a
laodaming <1059642220@qq.com>
2021-03-30 15:56:12 +0800

    fxi

--- database/mysqldb.go
+++ database/mysqldb.go
@@ -10,27 +10,27 @@ import (
 //操作数据库指针
 var myDb *gorm.DB
 var once sync.Once
-func init(){
-	once.Do(func(){
-		connectDb()
-	})
-}
 func connectDb(){
 	var err error
-	var Dsn = config.C("DB_USER")+":"+ config.C("DB_PWD")+"@("+config.C("DB_HOST")+")/"+config.C("DB_NAME")+"?charset=utf8&parseTime=true"
-	if myDb,err = gorm.Open(config.C("DB_DRIVER"),Dsn);err != nil{
-		fmt.Println(err)
-		return
-	}
-	//myDb.LogMode(true)//logdebug打印sql
-	fmt.Println("success to connect db")
-	//数据表设置为单数
-	myDb.SingularTable(true)
-	//空闲时连接数
-	myDb.DB().SetMaxIdleConns(10)
-	//最大连接数
-	myDb.DB().SetMaxOpenConns(100)
+	once.Do(func(){
+		var Dsn = config.C("DB_USER")+":"+ config.C("DB_PWD")+"@("+config.C("DB_HOST")+")/"+config.C("DB_NAME")+"?charset=utf8&parseTime=true"
+		if myDb,err = gorm.Open(config.C("DB_DRIVER"),Dsn);err != nil{
+			fmt.Println(err)
+			return
+		}
+		//myDb.LogMode(true)//logdebug打印sql
+		fmt.Println("success to connect db")
+		//数据表设置为单数
+		myDb.SingularTable(true)
+		//空闲时连接数
+		myDb.DB().SetMaxIdleConns(10)
+		//最大连接数
+		myDb.DB().SetMaxOpenConns(100)
+	})
 }
 func GetDb()*gorm.DB{
+	if myDb == nil{
+		connectDb()
+	}
 	return myDb
 }
