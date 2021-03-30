133eb44766ffb5e40f9a8c62b39e206f07e12df1
laodaming <1059642220@qq.com>
2021-03-30 16:17:48 +0800

    fix

--- config/config.go
+++ config/config.go
@@ -1,14 +1,20 @@
 package config
 
+import "sync"
+
 var mapConfig map[string] string
+var once sync.Once
 func init(){
-	mapConfig = make(map[string] string)
-	//数据库配置信息
-	mapConfig["DB_HOST"] = "127.0.0.1:3306"
-	mapConfig["DB_USER"] = "root"
-	mapConfig["DB_PWD"] = "123"
-	mapConfig["DB_NAME"] = "lytest"
-	mapConfig["DB_DRIVER"] = "mysql"
+	once.Do(func(){
+		mapConfig = make(map[string] string)
+		//数据库配置信息
+		mapConfig["DB_HOST"] = "127.0.0.1:3306"
+		mapConfig["DB_USER"] = "root"
+		mapConfig["DB_PWD"] = "123"
+		mapConfig["DB_NAME"] = "lytest"
+		mapConfig["DB_DRIVER"] = "mysql"
+	})
+
 }
 func C(key string) string{
 	if val,ok := mapConfig[key];!ok{
