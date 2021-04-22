24d3a152080345dd362d69a96fc1cbb5527096a1
laodaming <1059642220@qq.com>
2021-04-22 12:54:39 +0800

    fix

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 )
 
 func main(){
+    var r *gin.Engine
 	//发布模式
 	gin.SetMode(gin.ReleaseMode)
 	//注册路由
-	r := router.SetupRouter()
+	router.SetupRouter(r)
 	//挂起tcp服务端
 	go network.SetUpTcpServer()
 	//挂起tcp客户端
--- router/router.go
+++ router/router.go
@@ -8,8 +8,8 @@ import (
 func noRouter(c *gin.Context){
 	c.String(404,"无效的访问路由")
 }
-func SetupRouter() *gin.Engine{
-	r := gin.Default()
+func SetupRouter(r *gin.Engine){
+	r = gin.Default()
 	r.Static("/static","./static")
 	r.LoadHTMLGlob("./view/*")
 	//设置访问路由
@@ -23,5 +23,4 @@ func SetupRouter() *gin.Engine{
 	//websocket连接升级请求
 	r.GET("/ws",network.WeChat)
 	r.NoRoute(noRouter)
-	return r
 }
