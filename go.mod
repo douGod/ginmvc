f94e59310700b5c285542809382455cd9d000dc2
laodaming <1059642220@qq.com>
2021-07-26 17:04:50 +0800

    增加jwt样例

--- go.mod
+++ go.mod
@@ -6,9 +6,9 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/golang-jwt/jwt v3.2.1+incompatible
 	github.com/gorilla/websocket v1.4.2
 	github.com/jinzhu/gorm v1.9.16
-	github.com/mojocn/base64Captcha v1.3.4
 	github.com/onsi/ginkgo v1.16.1 // indirect
 	github.com/onsi/gomega v1.11.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"LaodamingMVC/router"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"os"
+	"time"
 )
-
+func TestJwt(){
+	file ,_ := os.Open("C:\\Users\\Administrator\\Desktop\\a.txt")
+	defer file.Close()
+	fileData := make([]byte,512)
+	n,_ := file.Read(fileData)
+	fmt.Println(string(fileData[:n]))
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"foo": "bar",
+		"nbf": time.Now().Unix(),
+	})
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString(fileData[:n])
+	if err != nil{
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(tokenString)
+	token ,err = jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return fileData[:n],nil
+	})
+	if token.Valid{
+		fmt.Println(token.Claims.(jwt.MapClaims))
+	}
+}
 func main(){
 	//发布模式
 	gin.SetMode(gin.ReleaseMode)
